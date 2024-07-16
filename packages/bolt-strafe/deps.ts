aaaee5538acdda7dbb33ce58853481b17268e095
NightRaven <53287573+Xynnix@users.noreply.github.com>
2024-07-16 14:53:50 -0400

    Create bolt-strafe/deps.ts

---
+++ packages/bolt-strafe/deps.ts
@@ -0,0 +1,12 @@
+export {
+	type API,
+	Client,
+	type Message
+} from 'npm:@strafechat/strafe.js@1.4.59';
+export {
+	type bridge_channel,
+	type deleted_message,
+	type lightning,
+	type message,
+	plugin,
+} from '../lightning/mod.ts';
